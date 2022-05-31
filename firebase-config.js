8eaf3c7a5c9336709fd9132d770ee47812395b72
Disney <disneyandrade@gmail.com>
2022-05-31 13:37:53 -0300

    Fix Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,10 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+
+import "firebase/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
@@ -7,4 +13,15 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
-const app = initializeApp(firebaseConfig);
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export { auth, provider };
+export default db;
+
+
+// const app = initializeApp(firebaseConfig);
